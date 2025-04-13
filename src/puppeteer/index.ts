d79798f1f293ea9053fedab63b9f9caf44224d74
dddaisuke <dddaisuke@gmail.com>
2025-04-14 08:42:00 +0900

    起動中のブラウザを使用する

--- src/puppeteer/index.ts
+++ src/puppeteer/index.ts
@@ -158,7 +158,7 @@ async function ensureBrowser({ launchOptions, allowDangerous }: any) {
   if (!browser) {
     const npx_args = { headless: false }
     const docker_args = { headless: true, args: ["--no-sandbox", "--single-process", "--no-zygote"] }
-    browser = await puppeteer.launch(deepMerge(
+    browser = await puppeteer.connect(deepMerge(
       process.env.DOCKER_CONTAINER ? docker_args : npx_args,
       mergedConfig
     ));
